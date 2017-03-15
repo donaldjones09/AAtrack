e01012df0c594dbf133950f853a5e5cce78fc8ff
donaldjones09 <donald.jones600@gmail.com>
2017-03-15 15:23:06 -0400

    implemented new pages w/sqlalchemy start
    began to implement sqlalchemy, not done yet.
    New pages rankings, and lookup tool rank lookup combined with JS will
    allow live lookup without refresh

--- application.py
+++ application.py
@@ -1,5 +1,19 @@
 from flask import *
+from sqlalchemy import *
+
+app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/rankings")
+def rankings():
+    return render_template("rankings.html")
+
+@app.route("/ranklookup")
+def ranklookup():
+    data = request.args.get("q")
+    parameters = data.split(",")
+
+    return jsonify(ranks)
