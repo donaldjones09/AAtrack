9886827b7d4c4e1a655e40c446793cabde286f12
donaldjones09 <donald.jones600@gmail.com>
2017-03-15 16:51:31 -0400

    Commented ranklookup()

--- application.py
+++ application.py
@@ -10,10 +10,11 @@ def index():
 @app.route("/rankings")
 def rankings():
     return render_template("rankings.html")
-
+"""
 @app.route("/ranklookup")
 def ranklookup():
     data = request.args.get("q")
     parameters = data.split(",")
     #will return a json array called ranks to script.js to update rankings page
     return jsonify(ranks)
+"""
